6b4c02833c20a65fdc5f764f04b2297db4aa185f
mikemonari <83007733+mikemonari@users.noreply.github.com>
2024-06-18 22:30:49 +0300

    initial commit

---
+++ src/pages/Questionbank.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Questionbank = () => {
+  return <div>Questionbankd</div>;
+};
+
+export default Questionbank;
---
+++ src/pages/Studyplan.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Studyplan = () => {
+  return <div>dcdc</div>;
+};
+
+export default Studyplan;
